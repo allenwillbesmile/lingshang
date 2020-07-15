3d5946c212415a7dbb19f0e020f3ae864037d845
791832278@qq.com <791832278@qq.com>
2020-07-15 11:45:52 +0800

    lingshang

--- routes/db.js
+++ routes/db.js
@@ -1,13 +1,13 @@
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-var db=mysql.createConnection({
-    host:'localhost',
-    post:'3306',
-    user:"root",
-    password:'123456',
-    database:"lingshang",
-});
+// var db=mysql.createConnection({
+//     host:'localhost',
+//     post:'3306',
+//     user:"root",
+//     password:'123456',
+//     database:"lingshang",
+// });
 
 
 
-db.connect();
+// db.connect();
